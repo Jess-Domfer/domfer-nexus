0d176ec82ee1d34b51762cd183b45eacaa36783b
Jess Domfer <jessica.dominguezf@outlook.com>
2026-03-20 19:57:32 -0600

    feat new button component

--- libs/design-system/src/index.ts
+++ libs/design-system/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/design-system/design-system';
+
+export * from './lib/atom/button';
---
+++ libs/design-system/src/lib/atom/button.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-button',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Button { }
